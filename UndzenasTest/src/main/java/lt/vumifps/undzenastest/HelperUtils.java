b7353442dd06d8f56f5d4bfd1471e37b19f775a2
Tomas <tomuibar@gmail.com>
2013-12-22 16:46:13 +0200

    Added more helper utilities. Code refactoring.

--- UndzenasTest/src/main/java/lt/vumifps/undzenastest/HelperUtils.java
+++ UndzenasTest/src/main/java/lt/vumifps/undzenastest/HelperUtils.java
@@ -3,18 +3,28 @@ package lt.vumifps.undzenastest;
 
 import android.content.Context;
 import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class HelperUtils {
 
     public static int dpToPx(Context context, int dp) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-        int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
-        return px;
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 
     public static int pxToDp(Context context, int px) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-        int dp = Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
-        return dp;
+        return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    public static int getScreenWidthPx(Context context) {
+        Display d = ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        return d.getWidth();
+    }
+
+    public static int getScreenHeightPx(Context context) {
+        Display d = ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        return d.getHeight();
     }
 }
